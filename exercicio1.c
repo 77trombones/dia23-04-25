24e11c4973f63b2c4ef0152724f3025ac0ae9ad6
tavoavoavo <gustavovenanandrade@gmail.com>
2025-04-23 19:34:03 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    double n[5];
+
+    n[0] = 10.0;
+    n[1] = 8.5;
+    n[2] = 6.9;
+    n[3] = 7.6;
+    n[4] = 3.1;
+
+printf("vetor[0] = %f.\n" , n[0]);
+printf("vetor[1] = %f.\n" , n[1]);
+printf("vetor[2] = %f.\n" , n[2]);
+printf("vetor[3] = %f.\n" , n[3]);
+printf("vetor[4] = %f.\n" , n[4]);}
